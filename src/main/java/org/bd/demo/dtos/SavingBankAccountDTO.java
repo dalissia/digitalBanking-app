c103a49661a2915604d7ea72cdd913ec6fb3a013
dalissia <dalissiabenidir@gmail.com>
2024-08-09 22:27:47 +0200

    add dtos

---
+++ src/main/java/org/bd/demo/dtos/SavingBankAccountDTO.java
@@ -0,0 +1,15 @@
+package org.bd.demo.dtos;
+
+import lombok.Data;
+import org.bd.demo.enums.AccountStatus;
+
+import java.util.Date;
+@Data
+public class SavingBankAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double interestRate;
+}
