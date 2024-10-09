6a5dfe21d507401bb0a70326af96272d2047932b
dalissia <dalissiabenidir@gmail.com>
2024-10-09 11:39:46 +0200

    Add controller BankAccount

--- src/main/java/org/bd/demo/web/BankAccountRestAPI.java
+++ src/main/java/org/bd/demo/web/BankAccountRestAPI.java
@@ -1,14 +1,12 @@
 package org.bd.demo.web;
 
 
+import org.bd.demo.dtos.AccountHistoryDTO;
 import org.bd.demo.dtos.AccountOperationDTO;
 import org.bd.demo.dtos.BankAccountDTO;
 import org.bd.demo.exceptions.BankAccountNotFoundException;
 import org.bd.demo.services.BankAccountService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +31,14 @@ public class BankAccountRestAPI {
     public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
         return bankAccountService.accountHistory(accountId);
    }
+    @GetMapping("/accounts/{accountId}/pageOperations")
+    public AccountHistoryDTO getAccountHistory(
+            @PathVariable String accountId,
+            @RequestParam(name="page",defaultValue = "0") int page,
+            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
+        return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+
     }
 
 
